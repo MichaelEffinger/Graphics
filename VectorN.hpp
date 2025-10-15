0965fae6585a2352c5cf6e436ebfddd4c148590e
Michael Effinger <meffinger1231@gmail.com>
2025-10-15 16:09:46 -0500

    added template header for VectorN

--- VectorN.hpp
+++ VectorN.hpp
@@ -14,8 +14,42 @@ class VectorN{
 
     template <typename... Args> requires (sizeof...(Args) == N) &&(std::conjunction_v<std::is_convertible<Args,T>...>)
     
-    
     constexpr VectorN(Args... args);
+
+    T& operator[](int index);
+
+    const T& operator[](int index) const;
+
+    const bool operator==(const VectorN<T, N>& other)const;
+
+    VectorN<T,N> operator+(const VectorN<T,N>&other)const;
+
+    VectorN<T,N> operator-(const VectorN<T,N>&other)const;
+
+    VectorN<T,N> operator/(const T scalar)const;
+
+    VectorN<T,N> operator*(const T scalar)const;
+
+    VectorN<T,N> hadamardProduct(const VectorN<T,N>&other)const;
+
+    VectorN<T,N> hadamardDivide(const VectorN<T,N>&other)const;
+
+    T length();
+
+    const T angle(const VectorN<T,N>&other)const;
+
+    VectorN<T,N> normalized();
+
+    void normalize();
+
+    template<int M, std::enable_if_t<(M < N), int> = 0>
+    constexpr VectorN(const VectorN<T, M>& other);
+
+    template<int M, std::enable_if_t<(M > N), int> = 0>
+    explicit constexpr VectorN(const VectorN<T, M>& other);
+
+
+
 
 
 };
