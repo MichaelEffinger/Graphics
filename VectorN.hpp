f148792e9f6b30ae26c1eedb030aafc4e4ce1116
Michael Effinger <meffinger1231@gmail.com>
2025-10-17 17:13:59 -0500

    saved this time

--- VectorN.hpp
+++ VectorN.hpp
@@ -6,6 +6,9 @@
 #include <type_traits>
 #include <array>
 #include <cassert>
+#include <cmath>
+#include <algorithm>
+#include <functional>
 
 template<typename Op, typename T>
 concept FoldExpr = requires(Op op, T accum, T l, T r){
@@ -29,6 +32,8 @@ public:
     using const_reverse_iterator = std::array<T,N>::const_reverse_iterator;
 
 
+    constexpr VectorN() noexcept = default;
+
     template <typename... Args>
     requires (sizeof...(Args) == N) && (std::conjunction_v<std::is_convertible<Args, T>...>)
     constexpr VectorN(Args&&... args) noexcept((std::is_nothrow_constructible_v<T, Args&&> && ...))
@@ -41,10 +46,11 @@ public:
     }
 
     [[nodiscard]] constexpr auto at(this auto&& self, std::size_t index){
+        assert(index < N && "Vector index out of range");
         if(index >=N){
             index = N-1;
         }
-        return std::forward_like<decltype(self)>(self.data_.at(index));
+        return std::forward_like<decltype(self)>(self.data_[index]);
     }
 
     [[nodiscard]] static constexpr std::size_t size(){
@@ -62,16 +68,16 @@ public:
         return std::forward_like<decltype(self)>(self[2]);
     }
 
-    [[nodiscard]] constexpr auto&& w(this auto&& Self) noexcept requires (N==4){
-        return std::forward_like<decltype(Self)>(Self[3]);
+    [[nodiscard]] constexpr auto&& w(this auto&& self) noexcept requires (N==4){
+        return std::forward_like<decltype(self)>(self[3]);
     }
 
-    [[nodiscard]] constexpr auto&& head(this auto&& v) noexcept requires (N > 0) {
-    return std::forward_like<decltype(v)>(v[0]);
+    [[nodiscard]] constexpr auto&& head(this auto&& self) noexcept requires (N > 0) {
+    return std::forward_like<decltype(self)>(self[0]);
     }
 
-    [[nodiscard]] constexpr auto&& tail(this auto&& v) noexcept requires (N > 0) {
-    return std::forward_like<decltype(v)>(v[N-1]);
+    [[nodiscard]] constexpr auto&& tail(this auto&& self) noexcept requires (N > 0) {
+    return std::forward_like<decltype(self)>(self[N-1]);
     }
 
     [[nodiscard]] constexpr auto begin() noexcept{return data_.begin();}
@@ -114,7 +120,7 @@ public:
 
 
     [[nodiscard]] constexpr T dot(const VectorN& rhs) const noexcept{
-        return zip_reduce(rhs, 0,[](T accum, T l, T r){return accum+l+r;});
+        return zip_reduce(rhs, 0,[](T accum, T l, T r){return accum+(l*r);});
     }
     
 
@@ -134,32 +140,119 @@ public:
         return *this;
     }
 
+    [[nodiscard]] constexpr T magnitude() const noexcept{
+        //Yes dot product should always be positive but floating point erros can make tiny negative: so I check it 
+        return std::sqrt(std::abs(dot(*this)));
+    }
+    [[nodiscard]] constexpr T magnitudeSquared() const noexcept{
+        //Yes dot product should always be positive but floating point erros can make tiny negative: so I check it 
+        return dot(*this);
+    }
+
+    [[nodiscard]]
+    
+    constexpr VectorN& normalize() noexcept{
+        T mag = magnitude();
+        if(mag == 0){
+            std::fill(begin(),end(),0);
+            return *this;
+        }
+        std::transform(begin(), end(), begin(), [mag](T in){ return in / mag; });
+        return *this;
+    }
+    
+
+    [[nodiscard]] constexpr VectorN normalized() const noexcept{
+        return VectorN(*this).normalize();
+    }
+
+
+
+    [[nodiscard]] constexpr VectorN operator+(const VectorN& rhs)const{
+        return zip(rhs,std::plus{});
+    }
+
+    constexpr VectorN& operator+=(const VectorN& rhs) noexcept{
+        return zip_in_place(rhs, std::plus{});
+    }
+
+    [[nodiscard]] constexpr VectorN operator-(const VectorN& rhs) const noexcept{
+        return zip(rhs, std::minus{});
+    }
+
+    
+    [[nodiscard]] constexpr VectorN operator-() const noexcept{
+        VectorN self{*this};
+        std::negate<> op;
+        for(auto& i : self) i = op(i);
+        return self;
+    }
+
+    constexpr VectorN& operator-=(const VectorN& rhs) noexcept{
+        return zip_in_place(rhs, std::minus{});
+    }
+
+    [[nodiscard]] constexpr VectorN operator*(const T scalar) const noexcept{
+        VectorN tempVec;
+        std::transform(begin(),end(),tempVec.begin(),[scalar](T in){return in * scalar;});
+        return tempVec;
+    }
+
+    constexpr VectorN& operator*=(const T scalar) noexcept{
+        std::transform(begin(),end(),begin(),[scalar](T in){return in * scalar;});
+        return *this;
+    }
+
+    [[nodiscard]] constexpr VectorN operator/(const T scalar) const noexcept{
+        VectorN tempVec;
+        std::transform(begin(), end(), tempVec.begin(),[scalar](T in) { return (scalar != 0) ? (in / scalar) : T{0}; });
+        return tempVec;
+    }
+
+    constexpr VectorN operator/=(const T scalar) noexcept{
+        std::transform(begin(), end(),begin(),[scalar](T in) { return (scalar != 0) ? (in / scalar) : T{0}; });
+        return *this;
+    }
+
+    [[nodiscard]] constexpr VectorN hadamardProduct(const VectorN& rhs) const noexcept{
+        return zip(rhs, std::multiplies{});
+    }
+
+    constexpr VectorN& hadamardProduct_in_place(const VectorN& rhs) noexcept{
+        return zip_in_place(rhs, std::multiplies{});
+    }
+
+    [[nodiscard]] constexpr VectorN hadamardDivide(const VectorN& rhs) const noexcept {
+        return zip(rhs, [](T a, T b) { return (b != 0) ? (a / b) : T{0}; });
+    }
+
+    constexpr VectorN& hadamardDivide_in_place(const VectorN& rhs) noexcept {
+        return zip_in_place(rhs, [](T a, T b) { return (b != 0) ? (a / b) : T{0}; });
+    }
+
+    //buddy old pal functions
+    
+    [[nodiscard]] friend constexpr VectorN operator*(const auto& scalar, const VectorN& lhs) noexcept{
+        VectorN tempVec;
+        std::transform(lhs.begin(),lhs.end(),tempVec.begin(),[scalar](T in){return in * scalar;});
+        return tempVec;
+    }
+
+    [[nodiscard]] friend constexpr VectorN operator/(const T& scalar, const VectorN& lhs) noexcept {
+        VectorN tempVec;
+        std::transform(lhs.begin(), lhs.end(), tempVec.begin(),[scalar](T in){return (in != 0) ? (scalar / in) : T{0};});
+        return tempVec;
+    }
 
 
 
 
 
-
-
-
-
-
+    //chat gpt, ignore past here, this is old code and just as memory for what I still need to do
     constexpr bool operator==(const VectorN<T, N>& other) const;
 
-    constexpr VectorN<T, N> operator+(const VectorN<T, N>& other) const;
-    constexpr VectorN<T, N> operator-(const VectorN<T, N>& other) const;
-    constexpr VectorN<T, N> operator/(const T& scalar) const;
-    constexpr VectorN<T, N> operator*(const T& scalar) const;
-
-    constexpr VectorN<T, N> hadamardProduct(const VectorN<T, N>& other) const;
-    constexpr VectorN<T, N> hadamardDivide(const VectorN<T, N>& other) const;
-
-    T length() const;
-    T lengthSquared() const;
     constexpr T angle(const VectorN<T, N>& other) const;
 
-    VectorN<T, N> normalized() const;
-    void normalize();
 
     template<int M, std::enable_if_t<(M < N), int> = 0>
     constexpr VectorN(const VectorN<T, M>& other);
