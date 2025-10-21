4dba07afa7682e91d582e1f7cf840f5d03503ff9
Michael Effinger <meffinger1231@gmail.com>
2025-10-20 19:07:10 -0500

    Update VectorP.hpp
    here ya go

--- VectorP.hpp
+++ VectorP.hpp
@@ -2,6 +2,10 @@
 
 #include "VectorN.hpp"
 #include "VectorH.hpp"
+#include <memory>
+
+// GPU-safe padded vec3 (16-byte aligned)
+
 
 
 struct alignas(8) Vector2P: public VectorN<float,2>{};
@@ -14,12 +18,27 @@ struct alignas(16) Vector3P : public VectorN<float, 3> {
 
     constexpr Vector3P(float x, float y, float z) : VectorN<float, 3>({x, y, z}), pad_(1.0f) {}
 
+
     constexpr Vector3P(VectorN<float,3>);
 
-    constexpr VectorH toVectorH();
+    VectorH toVectorH(float W) const noexcept {
+        VectorH h = *reinterpret_cast<const VectorH*>(this);
+        h.w() = W;
+        return h;
+    }
+
+    VectorH toVectorH_In_Place(float W) const noexcept {
+        VectorH h = *reinterpret_cast<const VectorH*>(this);
+        h.w() = W;
+        return h;
+    }
+
+    VectorH& toVectorH_In_Place() noexcept {
+        return *reinterpret_cast<VectorH*>(this);
+    }
+
+
 
 };
-
-
 struct alignas(16) Vector4P: public VectorN<float,4>{};
 
