be5a367602f238c40e4239b5da2464478e532ca5
Michael Effinger <meffinger1231@gmail.com>
2025-11-04 21:51:04 -0600

    merged into main

--- ComputerGraphics.cpp
+++ ComputerGraphics.cpp
@@ -2,19 +2,11 @@
 //
 
 #include "ComputerGraphics.h"
-#include "Vector2.hpp"
-#include "Vector3.hpp"
-#include "HVector.hpp"
-
-using namespace std;
+#include "VectorN.hpp"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
 	return 0;
-
-	Vector2<int> myVec2(0,0);
-	Vector3<int> myVec3(0,0,0);
-	HVector<int> myHVec(0,0,0,0);
 	
 }
