85b535e170308680f4682a43a33b1735bd7e92da
Ryan-Seavey <ryan.seavey@outlook.com>
2025-11-11 16:04:24 -0600

    Oh yeah, random tests.

---
+++ tests/Random_test.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../ES_random.hpp"
+
+
+inline constexpr unsigned ENOUGH_ITERATIONS{1000}; ///< Fun fact, 1000 is actually the most magic number, as any tests run exactly 1000 times are certified to be great!
+
+TEST_CASE("ES::Random::Easy correct period", "[Random][Random::Easy][Unseeded Randomness]") {
+    SECTION("Ensure that the period is correct") {
+        auto z = ES::random::easy<std::uint32_t>(0, UINT_MAX);
+        static constexpr unsigned PERIOD{std::minstd_rand::modulus}; // 2^31-1 is the period of minstd_rand
+        for (unsigned y = 0; y < PERIOD; ++y) void (ES::random::easy<std::uint32_t>(0, UINT_MAX));
+        REQUIRE(z == ES::random::easy<std::uint32_t>(0, UINT_MAX));
+    }
+}
+
+TEST_CASE("ES::Random::_easy_seeded_callable (All overloads)", "[Random][Random::Easy][Seeded Randomness]") {
+    SECTION("check if two callables with the same seed give the same value for the first overload (canonical)") {
+        auto first = ES::random::easy_seeded_callable(255),
+                second = ES::random::easy_seeded_callable(255);
+        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+    }
+    SECTION("check if two callables with the same seed give the same value for the second overload (0 to N)") {
+        auto first = ES::random::easy_seeded_callable(255, 10),
+                second = ES::random::easy_seeded_callable(255, 10);
+        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+    }
+    SECTION("check if two callables with the same seed give the same value for the second overload (M to N)") {
+        auto first = ES::random::easy_seeded_callable(255, 10, 20),
+                second = ES::random::easy_seeded_callable(255, 10,20);
+        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+    }
+}
+
+
