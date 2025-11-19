a343e5b2c5b66aaecc8d2c7935cd7fb106e82766
Ryan-Seavey <ryan.seavey@outlook.com>
2025-11-19 02:48:14 -0600

    Polished the random tests via checks and Catch2's generators (which are purportedly better than loops...)!

--- tests/Random_test.cpp
+++ tests/Random_test.cpp
@@ -1,4 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
 #include "../ES_random.hpp"
 #include "ES_test_util.hpp"
 #include <climits>
@@ -14,19 +16,22 @@ TEST_CASE("ES::Random::Easy correct period", "[Random][Random::Easy][Unseeded Ra
 
 TEST_CASE("ES::Random::_easy_seeded_callable (All overloads)", "[Random][Random::Easy][Seeded Randomness]") {
     SECTION("check if two callables with the same seed give the same value for the first overload (canonical)") {
-        auto first = ES::random::easy_seeded_callable(255),
-                second = ES::random::easy_seeded_callable(255);
-        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+        unsigned i = GENERATE(range(0u, ENOUGH_ITERATIONS));
+        auto first = ES::random::easy_seeded_callable(i),
+                second = ES::random::easy_seeded_callable(i);
+        CHECK(first() == second());
     }
     SECTION("check if two callables with the same seed give the same value for the second overload (0 to N)") {
-        auto first = ES::random::easy_seeded_callable(255, 10),
-                second = ES::random::easy_seeded_callable(255, 10);
-        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+        unsigned i = GENERATE(range(0u, ENOUGH_ITERATIONS));
+        auto first = ES::random::easy_seeded_callable(i, 10),
+                second = ES::random::easy_seeded_callable(i, 10);
+        CHECK(first() == second());
     }
     SECTION("check if two callables with the same seed give the same value for the second overload (M to N)") {
-        auto first = ES::random::easy_seeded_callable(255, 10, 20),
-                second = ES::random::easy_seeded_callable(255, 10,20);
-        for (unsigned i = 0; i < ENOUGH_ITERATIONS; ++i) REQUIRE(first() == second());
+        unsigned i = GENERATE(range(0u, ENOUGH_ITERATIONS));
+        auto first = ES::random::easy_seeded_callable(i, 10, 20),
+                second = ES::random::easy_seeded_callable(i, 10, 20);
+        CHECK(first() == second());
     }
 }
 
